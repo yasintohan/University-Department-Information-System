f1ea3e886e56ecd437153e9d55c192d7edadba67
Yasin Tohan <yasintohann@gmail.com>
2021-05-11 16:43:24 +0300

    Research Pages Added

---
+++ university-system-asp/university-system-asp/Controllers/ResearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace university_system_asp.Controllers
+{
+    public class ResearchController : Controller
+    {
+        // GET: Research
+        public ActionResult ResearchAreas()
+        {
+            return View();
+        }
+
+        public ActionResult ResearchProjects()
+        {
+            return View();
+        }
+    }
+}
