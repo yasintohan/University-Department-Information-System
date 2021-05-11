fe387943ce23d6cd1379c1ed4a6b7ee526682c3f
Yasin Tohan <yasintohann@gmail.com>
2021-05-11 20:11:52 +0300

    DBContext Updated

--- university-system-asp/university-system-asp/Models/Classes/DBContext.cs
+++ university-system-asp/university-system-asp/Models/Classes/DBContext.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace university_system_asp.Models.Classes
 {
-    public class DBContext
+    public class DBContext : DbContext
     {
+
+        public DbSet<AboutPage> aboutPages { get; set; }
+        public DbSet<Announce> announces { get; set; }
+        public DbSet<Carousel> carousels { get; set; }
+        public DbSet<Classroom> classrooms { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+        public DbSet<Events> events { get; set; }
+        public DbSet<Lab> labs { get; set; }
+        public DbSet<News> news { get; set; }
+        public DbSet<Office> offices { get; set; }
+        public DbSet<People> peoples { get; set; }
+        public DbSet<ResearchArea> researchAreas { get; set; }
+        public DbSet<ResearchProject> researchProjects { get; set; }
+        public DbSet<SocialIcon> socialIcons { get; set; }
+
+
     }
 }
